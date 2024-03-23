f7abe037359f2fa4bf0ca6c9667d7b313b6be38e
Avyact Jain <>
2024-03-23 23:41:15 +0530

    change machine

---
+++ target/debug/deps/rust_uniswap_server-ba7fa173f5e9c310.d
@@ -0,0 +1,11 @@
+/Users/avyactjain/code/rust-uniswap/target/debug/deps/librust_uniswap_server-ba7fa173f5e9c310.rmeta: src/server.rs src/config.rs src/uniswap_service.rs /Users/avyactjain/code/rust-uniswap/target/debug/build/rust-uniswap-7aef6de77678196e/out/uniswap_api.rs src/../abi/uni_v3_eth_usdc_pool.json
+
+/Users/avyactjain/code/rust-uniswap/target/debug/deps/rust_uniswap_server-ba7fa173f5e9c310.d: src/server.rs src/config.rs src/uniswap_service.rs /Users/avyactjain/code/rust-uniswap/target/debug/build/rust-uniswap-7aef6de77678196e/out/uniswap_api.rs src/../abi/uni_v3_eth_usdc_pool.json
+
+src/server.rs:
+src/config.rs:
+src/uniswap_service.rs:
+/Users/avyactjain/code/rust-uniswap/target/debug/build/rust-uniswap-7aef6de77678196e/out/uniswap_api.rs:
+src/../abi/uni_v3_eth_usdc_pool.json:
+
+# env-dep:OUT_DIR=/Users/avyactjain/code/rust-uniswap/target/debug/build/rust-uniswap-7aef6de77678196e/out
